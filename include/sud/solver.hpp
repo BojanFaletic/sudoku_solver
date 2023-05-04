5f5b41c77db3e1bb8d7849b6b2f7becd4d61e0b1
BojanFaletic <bojanf.1996@gmail.com>
2023-05-04 02:53:31 +0200

    something to sleep on

--- include/sud/solver.hpp
+++ include/sud/solver.hpp
@@ -8,10 +8,36 @@ namespace sud
 
     class Solver
     {
+    protected:
+        Sudoku &sudoku;
     public:
         Solver(Sudoku &sudoku);
 
         virtual status_e solve() = 0;
+    };
+
+    class RowSolver : public Solver
+    {
+    public:
+        RowSolver(Sudoku &sudoku);
+
+        status_e solve() override;
+    };
+
+    class ColSolver : public Solver
+    {
+    public:
+        ColSolver(Sudoku &sudoku);
+
+        status_e solve() override;
+    };
+
+    class BoxSolver : public Solver
+    {
+    public:
+        BoxSolver(Sudoku &sudoku);
+
+        status_e solve() override;
     };
 
 }; // namespace sud
--- src/sud/solver.cpp
+++ src/sud/solver.cpp
@@ -2,5 +2,73 @@
 
 namespace sud
 {
-    
+    Solver::Solver(Sudoku &sudoku) : sudoku(sudoku) {}
+    RowSolver::RowSolver(Sudoku &sudoku) : Solver(sudoku) {}
+    ColSolver::ColSolver(Sudoku &sudoku) : Solver(sudoku) {}
+    BoxSolver::BoxSolver(Sudoku &sudoku) : Solver(sudoku) {}
+
+
+    status_e RowSolver::solve()
+    {
+        for (square_t row = 0; row < SUDOKU_SIZE; row++)
+        {
+            for (square_t col = 0; col < SUDOKU_SIZE; col++)
+            {
+                if (sudoku.get(row, col) == 0)
+                {
+                    missing_t missing = sudoku.possible_items(row, col);
+                    if (missing.size() == 1)
+                    {
+                        sudoku.set(row, col, *missing.begin());
+                    }
+                }
+            }
+        }
+        return SUCCESS;
+    }
+
+    status_e ColSolver::solve()
+    {
+        for (square_t col = 0; col < SUDOKU_SIZE; col++)
+        {
+            for (square_t row = 0; row < SUDOKU_SIZE; row++)
+            {
+                if (sudoku.get(row, col) == 0)
+                {
+                    missing_t missing = sudoku.possible_items(row, col);
+                    if (missing.size() == 1)
+                    {
+                        sudoku.set(row, col, *missing.begin());
+                    }
+                }
+            }
+        }
+        return SUCCESS;
+    }
+
+    status_e BoxSolver::solve()
+    {
+        for (square_t row = 0; row < SUDOKU_SIZE; row += SUDOKU_BOX_SIZE)
+        {
+            for (square_t col = 0; col < SUDOKU_SIZE; col += SUDOKU_BOX_SIZE)
+            {
+                for (square_t i = 0; i < SUDOKU_BOX_SIZE; i++)
+                {
+                    for (square_t j = 0; j < SUDOKU_BOX_SIZE; j++)
+                    {
+                        if (sudoku.get(row + i, col + j) == 0)
+                        {
+                            missing_t missing = sudoku.possible_items(row + i, col + j);
+                            if (missing.size() == 1)
+                            {
+                                sudoku.set(row + i, col + j, *missing.begin());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return SUCCESS;
+    }
+
 } // namespace sud
